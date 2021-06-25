ab0e7111c51d23eabc84f768305f15ed79f51c27
Alexey432 <alexorl432@gmail.com>
2021-06-25 15:05:47 +0300

    Project Started

---
+++ script.js
@@ -0,0 +1,14 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+
+
+})
